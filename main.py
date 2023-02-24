a008533b77cb7ab6236bc31ec067a3074641cb51
sho-yoshikawa <syoshika@student.42tokyo.jp>
2023-02-24 16:26:51 +0900

    -f option added.

--- main.py
+++ main.py
@@ -1,28 +1,59 @@
 import requests
 import json
 import sys
+import os
 
-input_text = input()
-
-for argv in sys.argv:
-	option = argv[:2]
-	if option == "-s":
-		print(option)
-	if option == "-d":
-		print(option)
-	if option == "-f"
-		print(option)
 
 src_lang = "EN"
 dst_lang = "JA"
+input_text = None
 
-url = "https://api-free.deepl.com/v2/translate"
-headers = {'Authorization': 'DeepL-Auth-Key ???????????????????????????'}
-data = {'text': input_text, 'target_lang': dst_lang}
+def setFileOption(s):
+	global input_text
+	path = os.getcwd() + "/" + s
+	if not os.path.exists(path):
+		print("file doesn't exist.")
+		exit(1)
+	if not os.path.isfile(path):
+		print("invalid file.")
+		exit(2)
+	with open (path) as f:
+		input_text = f.read()
 
-res = requests.post(url=url, headers=headers, data=data)
+def setReverseOption():
+	global src_lang
+	global dst_lang
+	src_lang = "JA"
+	dst_lang = "EN"
 
-res_json = res.json()
-print("---------- translations ----------")
-print(res_json['translations'][0]['text'])
+def checkOptions(argv):
+	for i in range(len(argv)):
+		option = argv[i][:2]
+		if option == "-r":
+			setReverseOption()
+		if option == "-f":
+			setFileOption(argv[i + 1])
+
+def httpReq(input_text):
+	url = "https://api-free.deepl.com/v2/translate"
+	headers = {'Authorization': 'DeepL-Auth-Key b8f8bde9-4816-53cf-5bdd-b13bcc57f0e7:fx'}
+	data = {'text': input_text, 'target_lang': dst_lang}
+	res = requests.post(url=url, headers=headers, data=data)
+	res_json = res.json()
+	return res_json['translations'][0]['text']
+
+def output(ret):
+#	print("---------- translations ----------")
+	print(ret)
+
+def main():
+	global input_text
+	checkOptions(sys.argv)
+	if input_text is None:
+		input_text = input()
+	ret = httpReq(input_text)
+	output(ret)
+
+if __name__ == "__main__":
+	main()
 
